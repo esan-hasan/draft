78843c3325e30b4586cd5b9a2f3fd318a6348fd1
Esan Hasan <esan.hasan@students.mq.edu.au>
2021-10-05 20:25:05 +1100

    initial commit

---
+++ R/bank.R
@@ -0,0 +1,12 @@
+#' @title Bank Data.
+#'
+#' @description A dataset containing the arrival times and services times for
+#' 100 customers
+#'
+#' @format A data frame with 100 rows and 2 variables:
+#' \describe{
+#'   \item{arrival_time}{customer's arrival times}
+#'   \item{service_time}{how long each customer was served for}
+#'   ...
+#' }
+"bank"
