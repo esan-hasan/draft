50c265e75ff543664729283f6f27d236a0b1eef5
Esan Hasan <esan.hasan@students.mq.edu.au>
2021-10-05 20:34:07 +1100

    changed line 34 in Multiserver script from data.frame() to tibble()

--- tests/testthat/test-Multiserver.R
+++ tests/testthat/test-Multiserver.R
@@ -1,5 +1,5 @@
 test_that("test that the function works", {
   expect_type(Multiserver(1:3, 4:6, 1), "list")
   expect_error(Multiserver())
-  expect_output(str(Multiserver(1:3, 4:6, 1)), "3 obs")
+  expect_output(str(Multiserver(1:3, 4:6, 1)), "3 x 4")
 })
